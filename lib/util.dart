7b4e78a5ade40928037dc02c4e5e06f2841fec46
Nick <nick.albright@gmail.com>
2020-07-28 12:27:15 -0400

    Ran dartfmt on both lib files, and lengthened the pubspec.yaml description to be at least 60 characters long.

--- lib/util.dart
+++ lib/util.dart
@@ -26,6 +26,7 @@ Future<T> co<T>(key, FutureOr<T> action()) async {
 
     assert(identical(c, c2));
   }
+
   void catchError(ex, StackTrace st) {
     final c2 = _locks.remove(key);
     c.completeError(ex, st);
@@ -36,9 +37,7 @@ Future<T> co<T>(key, FutureOr<T> action()) async {
   try {
     final result = action();
     if (result is Future<T>) {
-      result
-        .then(then)
-        .catchError(catchError);
+      result.then(then).catchError(catchError);
     } else {
       then(result as T);
     }
@@ -48,6 +47,7 @@ Future<T> co<T>(key, FutureOr<T> action()) async {
 
   return c.future;
 }
+
 final _locks = new HashMap<dynamic, Completer>();
 
 /// skip the TickerCanceled exception
